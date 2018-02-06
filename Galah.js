0b9cc610caa559ae25970f31475392830720d0f7
Eric Steen <eric.steen@und.edu>
2018-02-06 13:41:18 -0600

    birb1

---
+++ Galah.js
@@ -0,0 +1,13 @@
+var x = 0;
+var birb = document.createElement("birb.gif").style.position="";
+draw = function(){
+	birb.style.position(x,100);
+var y = 5;
+x = x + y;
+if(x == 0){
+	y = 5;
+	}
+if(x == 1000){
+	y = -5;
+	}
+}
